388c3a45f0fa2705266ee110126f9ac70c76206a
joozzang <kkkkk_g4@naver.com>
2025-06-05 23:31:53 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// 2초 후 커서 애니메이션 제거
+setTimeout(() => {
+  const title = document.querySelector(".title");
+  title.style.borderRight = "none";
+}, 3000); // 타이핑 애니메이션 시간과 동일
+
+const title = document.querySelector(".title");
+const startBtn = document.querySelector(".start-button");
+
+// 타이핑 애니메이션은 2초, steps(4, end)
+// 끝난 후 버튼 보이도록 2초 뒤 실행
+setTimeout(() => {
+  startBtn.classList.add("show-button");
+}, 3200); // 타이핑 애니메이션 지속 시간과 동일하게 설정
